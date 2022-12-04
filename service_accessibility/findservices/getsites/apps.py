f50ea8bcf06d755b5063288f7e93364cfa91de5b
duraznoj <sashka.warner@gmail.com>
2022-12-03 21:36:25 -0800

    Add site data.
    Update requirements.txt
    
    Calculate distance for N sites
    
    Add calculation of travel distance
    
    Add marker popups
    
    Add comments
    
    Calculate straight line distances between sites and user supplied
    address (input via autocomplete search bar). Filter close
    distances and calculate travel distances between user address and close
    sites. Map the resulting sites.

--- service_accessibility/findservices/getsites/apps.py
+++ service_accessibility/findservices/getsites/apps.py
@@ -1,6 +1,12 @@
+import os
+from json import load
 from django.apps import AppConfig
-
 
 class GetsitesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "getsites"
+
+    # For if we need to do anything on application startup
+    def ready(self):
+        pass
+        
